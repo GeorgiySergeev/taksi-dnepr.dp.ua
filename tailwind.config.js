9554614dc6ba0d596373c30b10d9fa0c0f4f09e7
George <s.georgiymail@gmail.com>
2026-03-23 18:21:01 +0200

    3

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,26 @@ export default {
     extend: {
       colors: {
         brand: {
-          DEFAULT: '#facc15', // yellow-400
-          light: '#fde047',   // yellow-300
-          dark: '#eab308',    // yellow-500
+          DEFAULT: '#facc15',
+          light: '#fde047',
+          dark: '#eab308',
         },
       },
       fontFamily: {
-        sans: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+        mono: ['JetBrains Mono', 'SF Mono', 'monospace'],
       },
       maxWidth: {
-        'landing': '72rem',
+        landing: '64rem',
       },
       screens: {
-        'xs': '375px',
+        xs: '375px',
       },
       borderRadius: {
-        'card': '1rem',
+        card: '0.75rem',
       },
       boxShadow: {
-        'card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+        card: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
         'card-hover': '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
       },
     },
